93b9afa61c33f6240446c82d8f096434fea7919f
Leo Zbona <leonard.zbona@uoit.net>
2017-12-02 16:38:43 -0500

    Commot 10
    Added ball linear/Angular Velocity made box bigger and added shader to box

--- babylonJS/js/initScene.js
+++ babylonJS/js/initScene.js
@@ -25,7 +25,7 @@ function initScene() {
     scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
     // Create the camera
-    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(20, 10, 10), scene);
+    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(75, 75, 100), scene);
     camera.setTarget(new BABYLON.Vector3.Zero());
     camera.attachControl(canvas);
 
@@ -52,63 +52,76 @@ function initGame() {
 
     var floor = BABYLON.Mesh.CreateBox("floor", 1, scene);
     floor.scaling.y = 0.1;
-    floor.scaling.x = 50;
-    floor.scaling.z = 50;
-    floor.position.y = -25;
+    floor.scaling.x = 100;
+    floor.scaling.z = 100;
+    floor.position.y = -50;
     floor.material = redWireframeMaterial
-    var wall1 = BABYLON.Mesh.CreateBox("wall1", 1, scene); //left
-    wall1.scaling.y = 50;
-    wall1.scaling.x = 0.5;
-    wall1.scaling.z = 50;
-    wall1.position.x = -25;
     
+    var wall1 = BABYLON.Mesh.CreateBox("wall1", 1, scene); //left
+    wall1.scaling.y = 100;
+    wall1.scaling.x = 0.5;
+    wall1.scaling.z = 100;
+    wall1.position.x = -50;
     wall1.material = redWireframeMaterial
     var wall2 = BABYLON.Mesh.CreateBox("wall2", 1, scene); //right
-    wall2.scaling.y = 50;
+    wall2.scaling.y = 100;
     wall2.scaling.x = 0.5;
-    wall2.scaling.z = 50;
-    wall2.position.x = 25;
+    wall2.scaling.z = 100;
+    wall2.position.x = 50;
     
     wall2.material = redWireframeMaterial
     var wall3 = BABYLON.Mesh.CreateBox("wall2", 1, scene); //right
-    wall3.scaling.y = 50;
+    wall3.scaling.y = 100;
     wall3.scaling.z = 0.5;
-    wall3.scaling.x = 50;
+    wall3.scaling.x = 100;
     wall3.position.x = 0;
-    wall3.position.z = 25;
+    wall3.position.z = 50;
     
     wall3.material = redWireframeMaterial
     var wall4 = BABYLON.Mesh.CreateBox("wall2", 1, scene); //right
-    wall4.scaling.y = 50;
+    wall4.scaling.y = 100;
     wall4.scaling.z = 0.5;
-    wall4.scaling.x = 50;
+    wall4.scaling.x = 100;
     wall4.position.x = 0;
-    wall4.position.z = -25;
+    wall4.position.z = -50;
     
     wall4.material = redWireframeMaterial
 
     var ceiling = BABYLON.Mesh.CreateBox("ceiling", 1, scene);
     ceiling.scaling.y = 0.5;
-    ceiling.scaling.x = 50;
-    ceiling.scaling.z = 50;
-    ceiling.position.y = 25;
+    ceiling.scaling.x = 100;
+    ceiling.scaling.z = 100;
+    ceiling.position.y = 50;
     ceiling.material = redWireframeMaterial
 
     var ball = BABYLON.Mesh.CreateSphere("ball", 16, 2, scene);
     var ballMaterial = new BABYLON.StandardMaterial("ball", scene);
     ballMaterial.emissiveColor = new BABYLON.Color3(0.96, 1, 0);
     ball.material = ballMaterial;
+    ball.position.y = 13;
+
+    var ball2 = BABYLON.Mesh.CreateSphere("ball2", 12, 2, scene);
+    var ballMaterial2 = new BABYLON.StandardMaterial("ball2", scene);
+    ballMaterial2.emissiveColor = new BABYLON.Color3(0.5,3, 0);
+    ball.material = ballMaterial2;
     ball.position.y = 10;
 
-    var gravityVector = new BABYLON.Vector3(0, -9.81, 0);
+    var gravityVector = new BABYLON.Vector3(0, 0, 0);
     scene.enablePhysics(gravityVector);
     ball.physicsImpostor = new BABYLON.PhysicsImpostor(ball, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 1}, scene);
+    ball2.physicsImpostor = new BABYLON.PhysicsImpostor(ball2, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 1}, scene);
     floor.physicsImpostor = new BABYLON.PhysicsImpostor(floor, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
     wall1.physicsImpostor = new BABYLON.PhysicsImpostor(wall1, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
     wall2.physicsImpostor = new BABYLON.PhysicsImpostor(wall2, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
     wall3.physicsImpostor = new BABYLON.PhysicsImpostor(wall3, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
     wall4.physicsImpostor = new BABYLON.PhysicsImpostor(wall4, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
-    ceilingphysicsImpostor = new BABYLON.PhysicsImpostor(ceiling, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+    ceiling.physicsImpostor = new BABYLON.PhysicsImpostor(ceiling, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+
+    ball.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(0,0,0));
+    // ball2.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(20,15,3));
+
+    ball.physicsImpostor.setAngularVelocity(new BABYLON.Vector3(10,0,0))
+
 
     window.addEventListener("keydown", onKeyDown);
 }
