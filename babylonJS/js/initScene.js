6247c001fa4e937df26474114ee4cfd230edc426
Kevin Desousa <kevin.desousa1@uoit.net>
2017-12-02 14:40:25 -0500

    Fixed skybox, camera and color

--- babylonJS/js/initScene.js
+++ babylonJS/js/initScene.js
@@ -23,9 +23,19 @@ function initScene() {
     // Create scene
     scene = new BABYLON.Scene(engine);
     // Create the camera
-    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 4, -10), scene);
+    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 10, -40), scene);
     camera.setTarget(new BABYLON.Vector3.Zero());
     camera.attachControl(canvas);
+
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 20.0, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/cubemap/skybox", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.alpha = 0.5;
+    skybox.material = skyboxMaterial;
 
     // Create light
     var light = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 5, -5), scene);
@@ -41,18 +51,9 @@ function initScene() {
 }
 
 function initGame() {
-    var skybox = BABYLON.Mesh.CreateBox("skyBox", 20.0, scene);
-    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
-    skyboxMaterial.backFaceCulling = false; 
-    skyboxMaterial.diffuseColor = new BABYLON.Color3(0,0,0);
-    skyboxMaterial.alpha = 0.1;
-    skyboxMaterial.emissiveColor  = new BABYLON.Color3(0.14,.39,.8);
-    skybox.material = skyboxMaterial;
-
     var ball = BABYLON.Mesh.CreateSphere("sphere", 16, 1, scene);
     var ballMaterial = new BABYLON.StandardMaterial("sphere", scene);
-    skyboxMaterial.backFaceCulling = false; 
-    ballMaterial.emissiveColor  = new BABYLON.Color3(0.96,1,0);
+    ballMaterial.emissiveColor = new BABYLON.Color3(0.96, 1, 0);
     ball.material = ballMaterial;
 
     window.addEventListener("keydown", onKeyDown);
@@ -60,34 +61,5 @@ function initGame() {
 
 
 function onKeyDown(event) {
-    switch (event.keyCode) {
-        case 68:
-            camera.position.x -= 0.5;
-            break;
-        case 65:
-            camera.position.x += 0.5;
-            break;
-        case 87:
-            camera.position.y -= 0.5;
-            break;
-        case 83:
-            camera.position.y += 0.5;
-            break;
-        case 81:
-            camera.position.z -= 0.5;
-            break;
-        case 69:
-            camera.position.z += 0.5;
-            break;
-        case 82:
-            rotate -= 0.1;
-            camera.rotation = new BABYLON.Vector3(0, rotate, 0);
-            break;
-        case 84:
-            rotate += 0.1;
-            camera.rotation = new BABYLON.Vector3(0, rotate, 0);
-            break;
-
-    }
 }
 
