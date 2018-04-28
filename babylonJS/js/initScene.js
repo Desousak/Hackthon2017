7e47e42ba4b7dd21e7c093363b4524ad9c39c522
Leo Zbona <leonard.zbona@uoit.net>
2018-04-28 13:55:04 -0400

    Added ball spawner, fixed box clipping

--- babylonJS/js/initScene.js
+++ babylonJS/js/initScene.js
@@ -25,15 +25,13 @@ function initScene() {
     scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
     // Create the camera
-    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(900, 900, 900), scene);
+    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(500, 500, 500), scene);
     camera.setTarget(new BABYLON.Vector3.Zero());
     camera.attachControl(canvas);
 
 
     // Create light
-    // var light = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 5, -5), scene);
-    var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
-    var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+    var light = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 5, -5), scene);
 
     engine.runRenderLoop(function () {
         scene.render();
@@ -47,53 +45,104 @@ function initScene() {
 
 function initGame() {
 
+    var clearaMat = new BABYLON.StandardMaterial("clearMat", scene);
+    clearaMat.diffuseColor = new BABYLON.Color3(1, 1, 1);
+    clearaMat.alpha = 0.5;
 
-    scene.enablePhysics();
-    scene.collisionsEnabled  = true;
+    var floor = BABYLON.Mesh.CreateBox("floor", 1, scene);
+    floor.scaling.y = 0.1;
+    floor.scaling.x = 50;
+    floor.scaling.z = 50;
 
-    var greenMat = new BABYLON.StandardMaterial("greenMat", scene);
-    greenMat.diffuseColor = new BABYLON.Color3(0.83, 0.83, 0.83);
-    greenMat.alpha = 0.5;
+    var wall1 = BABYLON.Mesh.CreateBox("wall1", 1, scene); //left
+    wall1.scaling.y = 25;
+    wall1.scaling.x = 0.5;
+    wall1.scaling.z = 50;
+    wall1.position.x = -25;
+    wall1.position.y = 12.5;
+    wall1.material = clearaMat
+    var wall2 = BABYLON.Mesh.CreateBox("wall2", 1, scene); //right
+    wall2.scaling.y = 25;
+    wall2.scaling.x = 0.5;
+    wall2.scaling.z = 50;
+    wall2.position.x = 25;
+    wall2.position.y = 12.5;
+    wall2.material = clearaMat
+    var wall3 = BABYLON.Mesh.CreateBox("wall2", 1, scene); //right
+    wall3.scaling.y = 50;
+    wall3.scaling.z = 0.5;
+    wall3.scaling.x = 50;
+    wall3.position.x = 0;
+    wall3.position.z = 25;
+    wall3.material = clearaMat
 
-    var box = BABYLON.MeshBuilder.CreateBox("box", {height: 1000, width:1000, depth: 1000, sideOrientation: BABYLON.Mesh.BACKSIDE}, scene);
-    box.material = greenMat
+    var wall4 = BABYLON.Mesh.CreateBox("wall2", 1, scene); //right
+    wall4.scaling.y = 50;
+    wall4.scaling.z = 0.5;
+    wall4.scaling.x = 50;
+    wall4.position.x = 0;
+    wall4.position.z = -25;
+    wall4.material = clearaMat
+
+    var ceiling = BABYLON.Mesh.CreateBox("ceiling", 1, scene);
+    ceiling.scaling.y = 0.5;
+    ceiling.scaling.x = 50;
+    ceiling.scaling.z = 50;
+    ceiling.position.y = 25;
+    ceiling.material = clearaMat
+
+
+    var ball = BABYLON.Mesh.CreateSphere("ball", 16, 2, scene);
+    var ballMaterial = new BABYLON.StandardMaterial("ball", scene);
+    ballMaterial.emissiveColor = new BABYLON.Color3(0.96, 1, 0);
+    ball.material = ballMaterial;
+    ball.position.y = 10;
+
+
+
+    var gravityVector = new BABYLON.Vector3(0, 0, 0);
+    scene.enablePhysics(gravityVector);
+    ball.physicsImpostor = new BABYLON.PhysicsImpostor(ball, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 1}, scene);
+    floor.physicsImpostor = new BABYLON.PhysicsImpostor(floor, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+    wall1.physicsImpostor = new BABYLON.PhysicsImpostor(wall1, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+    wall2.physicsImpostor = new BABYLON.PhysicsImpostor(wall2, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+    wall3.physicsImpostor = new BABYLON.PhysicsImpostor(wall3, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+    wall4.physicsImpostor = new BABYLON.PhysicsImpostor(wall4, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+    ceilingphysicsImpostor = new BABYLON.PhysicsImpostor(ceiling, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
 
     var rand = function(min, max){ return Math.floor(Math.random()*(max-min+1)+min)};
-    var n = 300;
-    var balls = [299];
-    var ballMaterial = [299];
+    var n = 9;
+    var balls = [10];
+    var ballMaterial = [10];
 
-    for(var i = 0; i<n; i++){
-        var a = rand(1,10);
-        var b = rand(1,10);
-        var c = rand(0,1);
-        var d = rand(0,1);
-        var e = rand(0,1);
-        var x = rand(0,1);
-        var y = rand(0,1);
-        var xx = rand(1,10);
-        var yy = rand(1,10);
-        var zz = rand(1,10);
+    for(var i = 0; i<n; i++) {
+        var a = rand(1, 10);
+        var b = rand(1, 10);
+        var c = rand(0, 1);
+        var d = rand(0, 1);
+        var e = rand(0, 1);
+        var x = rand(0, 1);
+        var y = rand(0, 1);
+        var xx = rand(1, 10);
+        var yy = rand(1, 10);
+        var zz = rand(1, 10);
         //var ax = rand(1,10);
         //var ay = rand(1,10);
         //var az = rand(1,15);
-        balls[i] = new BABYLON.Mesh.CreateSphere("ball"+i.toString(), a, b, scene);
-        ballMaterial[i] = new BABYLON.StandardMaterial("ball"+i.toString(), scene);
-        ballMaterial[i].emissiveColor = new BABYLON.Color3(c,d,e);
+        balls[i] = new BABYLON.Mesh.CreateSphere("ball" + i.toString(), a, b, scene);
+        ballMaterial[i] = new BABYLON.StandardMaterial("ball" + i.toString(), scene);
+        ballMaterial[i].emissiveColor = new BABYLON.Color3(c, d, e);
         balls[i].material = ballMaterial[i];
         balls[i].position.y = 0;
         balls[i].checkCollisions = true
-        balls[i].physicsImpostor = new BABYLON.PhysicsImpostor(balls[i], BABYLON.PhysicsImpostor.SphereImpostor, { mass: x, restitution: y}, scene);
-        balls[i].physicsImpostor.setLinearVelocity(new BABYLON.Vector3(xx,yy,zz));
+        balls[i].physicsImpostor = new BABYLON.PhysicsImpostor(balls[i], BABYLON.PhysicsImpostor.SphereImpostor, {
+            mass: x,
+            restitution: y
+        }, scene);
+        balls[i].physicsImpostor.setLinearVelocity(new BABYLON.Vector3(xx, yy, zz));
         //balls[i].physicsImpostor.setAngularVelocity(new BABYLON.Vector3(ax,ay,az));
+
     }
-
-    // var gravityVector = new BABYLON.Vector3(0, 0, 0);
-    scene.collisionsEnabled = true
-    box.physicsImpostor = new BABYLON.physicsImpostor(box, BABYLON.physicsImpostor.BoxImpostor, {mass: 0, restitution:1}, scene)
-
-
-
 
     window.addEventListener("keydown", onKeyDown);
 }
